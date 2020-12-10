89c947272c9bfab727831a254b593441ad092b81
Dhadhazi <dhadhazi@gmail.com>
2020-12-10 09:06:30 +0100

    Part two done

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 with open("input.txt") as file:
     data = file.read().split("\n")
-    data = sorted(int(d) for d in data)
+    data = [int(d) for d in data]
+    data.append(0)
+    data = sorted(data)
+    data.append(max(data)+3)
 
 
-# 3 starts at one because built in adapter
-differences = {1: 0, 2: 0, 3: 1}
+differences = {1: 0, 2: 0, 3: 0}
 
-# Adding difference between start and first adapter
-differences[data[0]] += 1
 
 # Getting all the differences
 for i in range(len(data)-1):
@@ -15,3 +15,23 @@ for i in range(len(data)-1):
     differences[diff] +=1
 
 print(f"The 1st and 3rd differences multiplied: {differences[1]*differences[3]}")
+
+# Part Two
+
+ways = {}
+
+
+def way_calc(i):
+    if i == len(data)-1:
+        return 1
+    if i in ways:
+        return ways[i]
+    counter = 0
+    for j in range(i+1, len(data)):
+        if data[j]-data[i]<=3:
+            counter += way_calc(j)
+    ways[i] = counter
+    return counter
+
+
+print(f"The answer for part two: {way_calc(0)}")
